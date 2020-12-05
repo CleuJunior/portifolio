e3a18e73816e4d1a36abcfb2d68f02b736490339
Cleonildo Junior <cleu.junior@gmail.com>
2020-12-04 21:02:06 -0300

    package: calculo de oraçamento - Finalização do Projeto 04-12-2020

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 
 )
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semana(s)`
+    atualizarPreco()
+
+})
+
+
+function atualizarPreco() {
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+
+    let preco = qtde * 100
+    if (temJS) preco *= 1.1    
+    if(incluiLayout) preco += 500
+    let taxaDeUrgencia = 1 - prazo * 0.1
+    preco *= 1 + taxaDeUrgencia
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
